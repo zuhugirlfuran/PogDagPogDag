20fd1e81eeaabf31cc08dcda59a021aaba52ba17
bmlsj <gaga2353@gmail.com>
2024-11-19 20:58:48 +0900

    @Service 추가

--- snuggle_server/src/main/java/com/ssafy/snuggle/model/service/UserServiceImpl.java
+++ snuggle_server/src/main/java/com/ssafy/snuggle/model/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ssafy.snuggle.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.snuggle.model.dao.UserDao;
 import com.ssafy.snuggle.model.dto.User;
 
+@Service
 public class UserServiceImpl implements UserService{
 
 	@Autowired
