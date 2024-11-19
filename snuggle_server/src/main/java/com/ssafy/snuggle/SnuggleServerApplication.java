0e9e56a0b9a3f16c6641706cacb3ff623df7f36b
migyeong <rmfosem613@gmail.com>
2024-11-19 09:48:16 +0900

    init

---
+++ snuggle_server/src/main/java/com/ssafy/snuggle/SnuggleServerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.snuggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnuggleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnuggleServerApplication.class, args);
+	}
+
+}
