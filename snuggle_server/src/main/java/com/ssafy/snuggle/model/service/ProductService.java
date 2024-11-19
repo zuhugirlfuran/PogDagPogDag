5ab9a72095904fd0087bcabc0ae8c6da5b3319c2
migyeong <rmfosem613@gmail.com>
2024-11-19 17:57:01 +0900

    service 구현

--- snuggle_server/src/main/java/com/ssafy/snuggle/model/service/ProductService.java
+++ snuggle_server/src/main/java/com/ssafy/snuggle/model/service/ProductService.java
@@ -1,5 +1,24 @@
 package com.ssafy.snuggle.model.service;
 
-public interface ProductService {
+import java.util.List;
 
+import com.ssafy.snuggle.model.dto.Product;
+import com.ssafy.snuggle.model.dto.ProductWithComment;
+
+
+public interface ProductService {
+    /**
+     * 모든 상품 정보를 반환한다.
+     * @return
+     */
+    List<Product> getProductList();
+    
+    /**
+     * backend 관통 과정에서 추가됨
+     * 상품의 정보, 판매량, 평점 정보를 함께 반환
+     * @param productId
+     * @return
+     */
+    ProductWithComment selectWithComment(int productId);
+    
 }
