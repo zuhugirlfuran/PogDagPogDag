60395c781bbed6b554a675544db580dcab8c1fdb
bmlsj <gaga2353@gmail.com>
2024-11-21 00:38:45 +0900

    userController에 PostMapping info 추가

--- snuggle_server/src/main/java/com/ssafy/snuggle/controller/UserController.java
+++ snuggle_server/src/main/java/com/ssafy/snuggle/controller/UserController.java
@@ -68,13 +68,8 @@ public class UserController {
 	}
 
 	@PostMapping("/login")
-	@Operation(summary = "로그인 처리 후 성공적으로 로그인 되었다면 loginId라는 쿠키를 내려보낸다."  
-	, description = "<pre>userId와 password 두개만 넘겨도 정상동작한다. \n 아래는 id, pass만 입력한 샘플코드\n"
-    		+ "{\r\n"
-    		+ "  \"userId\": \"aa12\",\r\n"
-    		+ "  \"password\": \"aa12\"\r\n"
-    		+ "}"
-    		+ "</pre>")
+	@Operation(summary = "로그인 처리 후 성공적으로 로그인 되었다면 loginId라는 쿠키를 내려보낸다.", description = "<pre>userId와 password 두개만 넘겨도 정상동작한다. \n 아래는 id, pass만 입력한 샘플코드\n"
+			+ "{\r\n" + "  \"userId\": \"aa12\",\r\n" + "  \"password\": \"aa12\"\r\n" + "}" + "</pre>")
 	public User login(@RequestBody User user, HttpServletResponse response) throws UnsupportedEncodingException {
 		User selected = uService.login(user.getUserId(), user.getPassword());
 		if (selected != null) {
@@ -126,6 +121,22 @@ public class UserController {
 			info.put("userW", selected);
 			List<Order> orders = oService.getOrderByUser(selected.getUserId());
 			info.put("order", orders);
+			return info;
+		}
+	}
+
+	@PostMapping("/info")
+	@Operation(summary = "사용자의 정보와 함께 사용자의 주문 내역, 사용자 등급 정보를 반환한다.", description = "아래 User객체에서 id, pass 두개의 정보만 json으로 넘기면 정상동작한다.")
+	public Map<String, Object> getInfo(@RequestBody User user) {
+		User selected = uService.login(user.getUserId(), user.getPassword());
+		if (selected == null) {
+			return null;
+		} else {
+			Map<String, Object> info = new HashMap<>();
+			info.put("user", selected);
+			List<Order> orders = oService.getOrderByUser(user.getUserId());
+			info.put("order", orders);
+			// info.put("grade", getGrade(selected.getStamps()));
 			return info;
 		}
 	}
