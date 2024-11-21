6454fa8e6d644ef25a52049e621ef012f202554b
gaga9353 <gaga9353@naver.com>
2024-11-21 17:47:15 +0900

    bug: 초기 스플래쉬랑 커스텀 스플래쉬 모두 보임

--- snuggle_final_app/app/build.gradle.kts
+++ snuggle_final_app/app/build.gradle.kts
@@ -57,5 +57,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // splash
+    implementation (libs.androidx.core.splashscreen)
+
 
 }
