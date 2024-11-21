a836b266818dc70c91d8db0c519e23517b6650d8
gaga9353 <gaga9353@naver.com>
2024-11-21 15:21:25 +0900

    주문하기 및 댓글 다이얼로그 구현

--- snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/product/ProductDetailFragment.kt
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/product/ProductDetailFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.ssafy.snuggle_final_app.R
 import com.ssafy.snuggle_final_app.databinding.FragmentProductDetailBinding
 import com.ssafy.snuggle_final_app.dto.Comment
@@ -38,13 +41,44 @@ class ProductDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
 
+        // 댓글달기 버튼
+        binding.productDetailBtnComment.setOnClickListener {
+            showCommentDialog()
+        }
+
+        // 주문하기 버튼
+        binding.productDetailBtnOrder.setOnClickListener {
+            showOrderDialog()
+        }
+
+    }
+
+    private fun showOrderDialog() {
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        val dialogView = layoutInflater.inflate(R.layout.dialog_bottom_order, null)
+        bottomSheetDialog.setContentView(dialogView)
+        bottomSheetDialog.show()
+    }
+
+    private fun showCommentDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_add_comment, null)
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setView(dialogView)
+        val dialog = builder.create()
+
+        val dialogButton = dialogView.findViewById<Button>(R.id.dialog_button)
+        dialogButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun initAdapter() {
         val commentList = mutableListOf(
             Comment("너무 귀여워요!", 1, "id 01"),
-            Comment("키링으로 활용하기 좋아요", 1,"id 01"),
-            Comment("아기자기하고 어쩌구 저쩌구", 1,"id 01"),
+            Comment("키링으로 활용하기 좋아요", 1, "id 01"),
+            Comment("아기자기하고 어쩌구 저쩌구", 1, "id 01"),
             Comment("색 커스텀이 어쩌구", 1, "id 01")
         )
 
@@ -53,7 +87,6 @@ class ProductDetailFragment : Fragment() {
         binding.productDetailRecyclerview.adapter = adapter
         binding.productDetailRecyclerview.layoutManager = LinearLayoutManager(requireContext())
     }
-
 
 
     override fun onDestroyView() {
