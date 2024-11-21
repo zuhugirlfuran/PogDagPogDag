ddcd33d7886024057d8b5f8f569ae3a9991f3bcf
gaga9353 <gaga9353@naver.com>
2024-11-21 11:49:41 +0900

    상품 리사이클러뷰 구현 완료

---
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/data/Product.kt
@@ -0,0 +1,8 @@
+package com.ssafy.snuggle_final_app.data
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val price: String,
+    val imageResId: Int // 이미지 리소스 ID
+)
---
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/product/ProductFragment.kt
@@ -0,0 +1,64 @@
+package com.ssafy.snuggle_final_app.product
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import com.ssafy.snuggle_final_app.R
+import com.ssafy.snuggle_final_app.data.Product
+import com.ssafy.snuggle_final_app.databinding.FragmentProductBinding
+
+class ProductFragment : Fragment() {
+
+    private var _binding: FragmentProductBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var adapter: ProductAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProductBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+
+        val productList = mutableListOf(
+            Product(1, "푸딩거북이", "5000", R.drawable.product_btn_search),
+            Product(2, "상어인형", "8000",R.drawable.product_btn_search),
+            Product(3, "토끼인형", "7000",R.drawable.product_btn_search),
+            Product(4, "곰인형", "10000",R.drawable.product_btn_search)
+        )
+
+
+        adapter = ProductAdapter(productList) { product ->
+            Toast.makeText(requireContext(), "${product.name} 클릭됨", Toast.LENGTH_SHORT).show()
+        }
+
+
+        binding.productGridview.adapter = adapter
+        binding.productGridview.layoutManager = GridLayoutManager(requireContext(), 3)
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
