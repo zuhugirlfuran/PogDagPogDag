161f2c0eecf69aad4c39493fc827f2653f472d4a
migyeong <rmfosem613@gmail.com>
2024-11-22 10:45:48 +0900

    OrderFragment 구현

--- snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/order/OrderFragment.kt
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/order/OrderFragment.kt
@@ -1,53 +1,45 @@
-package com.ssafy.snuggle_final_app.main
+package com.ssafy.snuggle_final_app.order
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
 import com.ssafy.snuggle_final_app.MainActivity
 import com.ssafy.snuggle_final_app.R
-import com.ssafy.snuggle_final_app.databinding.FragmentBannerDetailBinding
-import com.ssafy.snuggle_final_app.databinding.FragmentMainBinding
-import com.ssafy.snuggle_final_app.order.OrderFragment
+import com.ssafy.snuggle_final_app.databinding.FragmentOrderBinding
 
-
-class BannerDetailFragment : Fragment() {
+class OrderFragment : Fragment() {
 
     private lateinit var mainActivity: MainActivity
-    private var _binding: FragmentBannerDetailBinding? = null
+    private var _binding: FragmentOrderBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mainActivity = activity as MainActivity
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentBannerDetailBinding.inflate(inflater, container, false)
-
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 툴바 숨기기
-        val toolbar = activity?.findViewById<Toolbar>(R.id.app_bar)
-        toolbar?.visibility = View.GONE
+        val mainActivity = activity as? MainActivity
+        mainActivity?.let {
+            it.findViewById<View>(R.id.bottomNavigation)?.visibility = View.GONE
+        }
 
-
-        binding.moveToOrder.setOnClickListener {
-            // OrderFragment로 이동
+        binding.orderBtn.setOnClickListener {
             val transaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.main_frameLayout, OrderFragment()) // `R.id.main_frameLayout`은 프래그먼트 교체할 컨테이너 ID
+            transaction.replace(R.id.main_frameLayout, OrderCompleteFragment()) // `R.id.main_frameLayout`은 프래그먼트 교체할 컨테이너 ID
             transaction.addToBackStack(null) // 이전 화면으로 돌아갈 수 있게 스택에 추가
             transaction.commit()
         }
