576a9c29aa4c91e528488b5cdd3949152040aadc
rmfosem613 <rmfosem613@gmail.com>
2024-11-24 11:49:09 +0900

    scanner 수정

--- snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/data/service/CategoryService.kt
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/data/service/CategoryService.kt
@@ -1,5 +1,6 @@
 package com.ssafy.snuggle_final_app.data.service
 
+import com.ssafy.snuggle_final_app.data.model.dto.Category
 import retrofit2.http.GET
 import retrofit2.http.Path
 import java.util.Locale
@@ -17,6 +18,6 @@ interface CategoryService {
     // 카테고리 id에 따라 카테고리 목록 불러오기
     // 그냥 전체 카테고리 목록을 불러오는게 낫지 않을까..
     @GET("/snuggle/category")
-    suspend fun getAllCategory(): List<Locale.Category>
+    suspend fun getAllCategory(): List<Category>
 
 }
