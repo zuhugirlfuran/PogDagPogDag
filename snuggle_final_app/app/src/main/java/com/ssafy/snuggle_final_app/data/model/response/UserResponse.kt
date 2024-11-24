81faf2befe9edfcb6f2d1fd4d993245ff3d3192f
rmfosem613 <rmfosem613@gmail.com>
2024-11-25 00:09:29 +0900

    마이페이지 사용자 정보 불러오는 중

--- snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/data/model/response/UserResponse.kt
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/data/model/response/UserResponse.kt
@@ -1,6 +1,8 @@
 package com.ssafy.snuggle_final_app.data.model.response
 
+import com.ssafy.snuggle_final_app.data.model.dto.Grade
+import com.ssafy.snuggle_final_app.data.model.dto.Order
 import com.ssafy.snuggle_final_app.data.model.dto.User
 
 
-data class UserResponse(val user: User)
+data class UserResponse(val user: User, var order:List<Order>)
