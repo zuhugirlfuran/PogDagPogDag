1c906d423ea3e2eb710cd53cc5ab6731037e0003
jeonghu1010 <jeonghu1010@naver.com>
2024-11-25 17:56:13 +0900

    주문 완료 후 네비게이션 반영되지 않는 현상 수정 - 노란 아이콘 위치 조정해야됨

--- snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/ui/order/OrderCompleteFragment.kt
+++ snuggle_final_app/app/src/main/java/com/ssafy/snuggle_final_app/ui/order/OrderCompleteFragment.kt
@@ -1,20 +1,26 @@
 package com.ssafy.snuggle_final_app.ui.order
 
+import BottomNavigationHelper
 import OrderViewModel
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ssafy.snuggle_final_app.R
 import com.ssafy.snuggle_final_app.base.ApplicationClass
 import com.ssafy.snuggle_final_app.base.BaseFragment
 import com.ssafy.snuggle_final_app.data.model.dto.Address
 import com.ssafy.snuggle_final_app.databinding.FragmentOrderCompleteBinding
 import com.ssafy.snuggle_final_app.ui.cart.OrderAdapter
+import com.ssafy.snuggle_final_app.ui.chatbot.ChatBotFragment
 import com.ssafy.snuggle_final_app.ui.main.MainFragment
 import com.ssafy.snuggle_final_app.ui.mypage.MypageFragment
+import com.ssafy.snuggle_final_app.ui.product.ProductFragment
+import com.ssafy.snuggle_final_app.ui.scanner.ScannerFragment
 import com.ssafy.snuggle_final_app.util.CommonUtils.makeComma
 
 private const val TAG = "OrderCompleteFragment"
@@ -54,8 +60,68 @@ class OrderCompleteFragment : BaseFragment<FragmentOrderCompleteBinding>(
             //Toast.makeText(requireContext(), "주문이 완료되었습니다.", Toast.LENGTH_SHORT).show()
             // 메인 화면으로 이동
             parentFragmentManager.beginTransaction()
-                .replace(R.id.main_frameLayout, MypageFragment())
+                .replace(R.id.main_frameLayout, MainFragment())
                 .commit()
+
+            // Navigation도 메인화면으로 돌리기
+            val bottomNavigationHelper = BottomNavigationHelper(
+                context = requireContext(),
+                fragmentManager = parentFragmentManager,
+                mainFrameId = R.id.main_frameLayout
+            )
+
+            bottomNavigationHelper.setup(
+                backIndicator = requireActivity().findViewById(R.id.backIndicator2),
+                tabs = listOf(
+                    Triple(
+                        requireActivity().findViewById(R.id.product),
+                        requireActivity().findViewById(R.id.productIndicator2),
+                        Triple(
+                            requireActivity().findViewById(R.id.productIcon2),
+                            requireActivity().findViewById(R.id.productLabel2),
+                            ProductFragment()
+                        )
+                    ),
+                    Triple(
+                        requireActivity().findViewById(R.id.scanner),
+                        requireActivity().findViewById(R.id.scannerIndicator2),
+                        Triple(
+                            requireActivity().findViewById(R.id.scannerIcon2),
+                            requireActivity().findViewById(R.id.scannerLabel2),
+                            ScannerFragment()
+                        )
+                    ),
+                    Triple(
+                        requireActivity().findViewById(R.id.home),
+                        requireActivity().findViewById(R.id.homeIndicator2),
+                        Triple(
+                            requireActivity().findViewById(R.id.homeIcon2),
+                            requireActivity().findViewById(R.id.homeLabel2),
+                            MainFragment()
+                        )
+                    ),
+                    Triple(
+                        requireActivity().findViewById(R.id.chatbot),
+                        requireActivity().findViewById(R.id.chatbotIndicator2),
+                        Triple(
+                            requireActivity().findViewById(R.id.chatbotIcon2),
+                            requireActivity().findViewById(R.id.chatbotLabel2),
+                            ChatBotFragment()
+                        )
+                    ),
+                    Triple(
+                        requireActivity().findViewById(R.id.mypage),
+                        requireActivity().findViewById(R.id.mypageIndicator2),
+                        Triple(
+                            requireActivity().findViewById(R.id.mypageIcon2),
+                            requireActivity().findViewById(R.id.mypageLabel2),
+                            MypageFragment()
+                        )
+                    )
+                ),
+                defaultTab = requireActivity().findViewById(R.id.home) // 기본 탭으로 Home 설정
+            )
+
         }
     }
 
@@ -74,6 +140,19 @@ class OrderCompleteFragment : BaseFragment<FragmentOrderCompleteBinding>(
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        activity?.findViewById<ConstraintLayout>(R.id.bottom_navigation)?.visibility = View.GONE
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        // Activity의 BottomNavigationView를 다시 보임
+        activity?.findViewById<ConstraintLayout>(R.id.bottom_navigation)?.visibility =
+            View.VISIBLE
+    }
     private fun navigateToMainFragment() {
         parentFragmentManager.beginTransaction()
             .replace(R.id.main_frameLayout, MainFragment())
